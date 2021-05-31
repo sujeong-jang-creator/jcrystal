9f93970bb85ebfc2d951646d416661c48c41e7dc
jcry <gomdorij@naver.com>
2021-05-31 15:01:28 +0900

    0531 오전수업

--- 03.SQL/3.selectFunction.sql
+++ 03.SQL/3.selectFunction.sql
@@ -15,31 +15,38 @@ select sysdate from dual;
 
 -- *** [숫자함수] ***
 -- 1. 절대값 구하는 함수 : abs()
-
+select abs(-5) from dual;
 
 
 -- 2. 반올림 구하는 함수 : round(데이터 [, 반올림자릿수])
-
+select 5.86, round(5.86), round(5.86,1) from dual;
+-
 
 
 -- 3. 지정한 자리수 이하 버리는 함수 : trunc()
 -- trunc(데이터, 자릿수)
 -- 자릿수 : +(소수점 이하), -(정수 의미)
 -- 참고 : 존재하는 table의 데이터만 삭제시 방법 : delete[복원]/truncate[복원불가]
+select 5.86, trunc(5.86), trunc(5.86,1) from dual;
+select trunc(5.987654321,2) from dual;
+select trunc(12345.987654321,-2) from dual;
 
   
 
 -- 4. 나누고 난 나머지 값 연산 함수 : mod()
 -- 모듈러스 연산자, % 표기로 연산, 오라클에선 mod() 함수명 사용
-
+select mod(7, 2) from dual;
 
 
 -- 5. ? emp table에서 사번(empno)이 홀수인 사원의 이름(ename), 사번(empno) 검색 
+select empno from emp;
 
-
+select empno, ename from emp where mod(empno, 2) = 1;
+select empno, ename from emp where mod(empno, 2) != 0;
 
 
 --6. 제곱수 구하는 함수 : power()
+select power(7, 2) from dual;
 
 
 
@@ -53,20 +60,38 @@ select sysdate from dual;
 -- 1. 대문자로 변화시키는 함수
 -- upper() : 대문자[uppercase]
 -- lower() : 소문자[lowercase]
+select upper('abCd') from dual;
+select lower('abCd') from dual;
+
+-- ? 사원명이 대문자로 되어 있으나 검색시 소문자로 변환
+-- emp table의 ename은 대문자로 유지
+select lower(ename) from emp;
+
+--2. ? manager로 job 칼럼과 뜻(대소문자 구분 없이)이 일치되는 사원의 사원명 검색하기 
+-- 소문자로 검색시 검색 불가
+
+select lower(job) from emp;
+
+select ename, job from emp
+where job = 'MANAGER';
 
 
+-- set timing on 으로 시간 체크 점검 후에 최종 sql 선택 권장
+-- 데이터가 많아야 실행 가치가 있음
 
+-- manager라는 하나의 문자열을 대문자로 변환해서 비교
+select ename, job from emp
+where job = upper('manager');
 
---2. ? manager로 job 칼럼과 뜻이 일치되는 사원의 사원명 검색하기 
-
-
+-- 모든 job의 데이터를 소문자로 변환해서 하나의 manager와 비교
+select ename, job from emp
+where lower(job) = 'manager';
 
 
 --3. 문자열 길이 체크함수 : length()
 -- 참고 사항 : oracle xe 버전은 단순 교육용 버전, 한글은 정석은 2byte 소진, xe 버전은 3byte 소진
-
-
 --4. byte 수 체크 함수 : lengthb()
+select length('a'), lengthb('a'), length('가'), lengthb('가') from dual;
 
 
 --5. 문자열 일부 추출 함수 : substr()
@@ -74,12 +99,18 @@ select sysdate from dual;
 -- *** 자바스크립트, 파이썬, 자바에서의 문자열 index(각음절의 위치 순서)는 0부터 시작, sql에선 1부터 시작
 
 -- substr(데이터, 시작위치, 추출할 개수)
-
+select 'playdata', substr('playdata', 0, 2) from dual;
 
 
 --6. ? 년도 구분없이 2월에 입사한 사원이름, 입사일 검색
 -- date 타입에도 substr() 함수 사용 가능 
 
+select hiredate from emp;
+select substr(hiredate, 4, 2) from emp; 
+select ename, hiredate from emp where substr(hiredate, 4, 2) = '02';
+
+
+select substr(hiredate, 4, 2) from emp; 
 
 
 
@@ -89,55 +120,68 @@ select sysdate from dual;
    trim() 호출해서 잉여 여백제거
    trim() 결과값으로 length() 실행 */
 
+select length(' abc '), length(trim(' abc ')) from dual;
 
+-- 8. ? emp table에서 두번째 철자가 I인 사람들 검색
+select ename
+from emp
+where ename like '_I%';
 
+select ename from emp where substr(ename, 2, 1) = 'I';
 
 -- *** [날짜 함수] ***
 --1. ?어제, 오늘, 내일 날짜 검색 
 -- 현재 시스템 날짜에 대한 정보 제공 속성 : sysdate
 
+select sysdate-1 as 어제, sysdate, sysdate+1 내일 from dual;
+
 
 --2.?emp table에서 근무일수 계산하기, 사번과 근무일수(반올림) 검색
+
+select empno, round(sysdate-hiredate) as 근무일수 from emp;
 
 
 --3. 특정 개월수 더하는 함수 : add_months()
 -- 6개월 이후 검색 
-
+select sysdate, add_months(sysdate, 6) from dual;
 
 
 --4. ? 입사일 이후 3개월 지난 일수 검색
-
+select sysdate, add_months(hiredate, 3) from emp;
 
 --5. 두 날짜 사이의 개월수 검색 : months_between()
 -- 오늘(sysdate) 기준으로 2016-09-19
-
+select months_between(sysdate, '2016-09-19') from dual;
+select months_between(sysdate, '2021-01-31') from dual;
 
 
 --6. 요일을 기준으로 특정 날짜 검색 : next_day()
-
+select next_day(sysdate, '토요일') from dual;
+select next_day(sysdate, '토') from dual;
 
 
 --7. 주어진 날짜를 기준으로 해당 달의 가장 마지막 날짜 : last_day()
-
-
+select last_day(sysdate) from dual;
+select last_day('2021-01-28') from dual;
 
 
 --8.? 2020년 2월의 마지막 날짜는?
-
-
+select last_day('2020-02-08') from dual;
+select last_day('2021-02-08') from dual;
+select last_day('2021/02/08') from dual;
 
 
 
 -- *** [형변환 함수] ***
 -- 사용 빈도가 높음
---[1] to_char() : 날짜 -> 문자, 숫자 -> 문자
+--[1] to_char() : 날짜(date) -> 문자(varchar2), 숫자(number) -> 문자(varchar2)
 	-- to_char(날자데이타, '희망포멧문자열')
 --[2] to_date() : 날짜로 변경 시키는 함수
 --[3] to_number() : 문자열을 숫자로 변환
 
 
 
--- [1] to_char()
+-- [1] to_char(데이터, '변경포멧')
 --1. 오늘 날짜를 'yyyy-mm-dd' 변환 : 
 select to_char(sysdate, 'yyyy/mm/dd') from dual;
 
@@ -160,7 +204,9 @@ select to_char(sysdate, 'yyyy-mm-dd hh:mi:ss am') from dual;
 -- 날짜의 round() : 정오를 기준으로 이 시간 초과시 무조건 다음 날짜
 --			   : 가령 12까지를 원서 접수 또는 택배마감등
 -- 날짜의 trunc() : 24시간 내의 모든 내용 당일 처리
+select sysdate, round(sysdate), trunc(sysdate) from dual;
 
+select sysdate, round(sysdate), to_char(trunc(sysdate) from dual
 
 
 -- 3. 숫자를 문자형으로 변환 : to_char()
@@ -180,12 +226,13 @@ select to_char(1234, '00000') from dual;
 select to_char(1234, 'L99,999') from dual;
 select to_char(1234, 'l99,999') from dual;
 
- 
 
 --[2] to_date() : 날짜로 변경 시키는 함수
 
 --1. 올해 며칠이 지났는지 검색(포멧 yyyy/mm/dd)
-select sysdate - 20200719 from dual;
+
+select to_date(20200719) from dual;
+select sysdate - to_date(20200719) from dual;
 
 
 
@@ -195,11 +242,14 @@ select ename from emp where hiredate='1980/12/17';
 select ename from emp where hiredate='80/12/17';
 select ename from emp where hiredate='1980-12-17';
 
-
 -- [3] to_number() : 문자열을 숫자로 변환
 --1. '20,000'의 데이터에서 '10,000' 산술 연산하기 
 -- 힌트 - 9 : 실제 데이터의 유효한 자릿수 숫자 의미(자릿수 채우지 않음)
 -- ?
+-- error : select '20,000' - '10,000' from dual;
+
+-- 문자열을 숫자로 변환시 포멧도 설정
+select to_number('20,000', '99,999') - to_number('10,000', '99,999') from dual;
 
 
 
@@ -211,17 +261,38 @@ select ename from emp where hiredate='1980-12-17';
 --			   default값) from table명;
 
 --1. deptno 에 따른 출력 데이터
+select * 
+from dept;
+
+-- 10번 부서인 경우 A등급, 20번 부서는 B등급, 나머지는 C등급으로 출력
+select deptno, decode(deptno, 10, 'A등급',
+                              20, 'B등급',
+                                  'C등급') 
+from dept;
 
 
 
 --2. emp table의 연봉(sal) 인상계산
 -- job이 ANALYST 5%인상(sal*1.05), SALESMAN 은 10%(sal*1.1) 인상, MANAGER는 15%(sal*1.15), CLERK 20%(sal*1.2) 이상 
 
-
-
+select ename, sal, decode(job, 'ANALYST', sal*1.05
+                                 'SALESMAN', sal*1.1,
+                                'MANAGER', sal*1.15,
+                                 'CLERK', sal*1.2); as 인상분
+from emp;
 
 --3. 'MANAGER'인 직군은 '갑', 'ANALYST' 직군은 '을', 나머지는 '병'으로 검색
 
 
 
+-- 1. locations table 에서 country_id가 US에 해당하는 city를 출력해 주세요.
+SELECT city, country_id
+FROM locations
+WHERE country_id = 'US';
+
+-- 2. locations table 에서 postal_code에 'Y'가 들어가는 city를 city 기준 오름차순으로 출력해 주세요. (단, city는 대문자로 출력)
+SELECT upper(city), postal_code
+FROM locations
+WHERE postal_code LIKE '%S%'
+ORDER BY city ASC;
 
